58e11e07813577a9d181210c859def089f07263b
tomek.belkowski@gmail.com <Thomas Belkowski>
2013-11-08 01:10:06 +0100

    fixed bug with formID

--- system/modules/xefg_uniqueid/efgFormId.php
+++ system/modules/xefg_uniqueid/efgFormId.php
@@ -4,19 +4,19 @@ class efgFormId extends Backend
 {
 	public function createFormId($arrSubmitted, $arrFiles, $intOldRecordId, $arrFormConfig) 
 	{
+		$isFormId = strpos($arrSubmitted['FORM_SUBMIT'], "auto_form");
+		$id = substr($arrSubmitted['FORM_SUBMIT'],($isFormId === false ? 5 : 10));
 		
-		$id = substr($arrSubmitted['FORM_SUBMIT'],5);		
-				
 		$this->Database
-				->prepare("UPDATE tl_form SET uniqueid = uniqueid + 1 WHERE formId = ?")
+				->prepare("UPDATE tl_form SET uniqueid = uniqueid + 1 WHERE ".($isFormId === false ? "formID" : "id")." = ?")
 				->execute($id);
 										
 		$f = $this->Database
-					->prepare("SELECT uniqueid FROM tl_form WHERE formId = ?")
+					->prepare("SELECT uniqueid FROM tl_form WHERE ".($isFormId === false ? "formID" : "id")." = ?")
 					->limit(1)
 					->execute($id);
 		
-		$arrSubmitted['uniqueid'] = $arrSubmitted['uniqueid'].str_pad($f->uniqueid,1,'0',STR_PAD_LEFT);	
+		$arrSubmitted['uniqueid'] = $arrSubmitted['uniqueid'].str_pad($f->uniqueid,1,'0',STR_PAD_LEFT);			
 		
 		return $arrSubmitted;
 	}
